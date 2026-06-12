479a517ec521d4d46441b45fbaafad269a13dabe
Marios Giagkou <mariosyiangou99@gmail.com>
2026-06-12 10:40:29 +0300

    feat: create MenuPageBody and add leg1.jpeg image to menu layout

--- oasis/lib/pages/menu_page.dart
+++ oasis/lib/pages/menu_page.dart
@@ -1,39 +1,84 @@
 import 'package:flutter/material.dart';
+import '../data/spa_theme.dart';
 
 class MenuPageBody extends StatelessWidget {
   const MenuPageBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(maxWidth: 800),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            Image.asset(
-              'lib/menu/menu0.png',
-              width: double.infinity,
-              fit: BoxFit.fitWidth,
-            ),
-            Image.asset(
-              'lib/menu/menubody.png',
-              width: double.infinity,
-              fit: BoxFit.fitWidth,
-            ),
-            Image.asset(
-              'lib/menu/leg.jpeg',
-              width: double.infinity,
-              fit: BoxFit.fitWidth,
-            ),
-            Image.asset(
-              'lib/menu/aaaa.png',
-              width: double.infinity,
-              fit: BoxFit.fitWidth,
-            ),
-          ],
-        ),
-      ),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth <= 800) {
+          return ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              Image.asset(
+                'lib/menu/menu0.png',
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+              ),
+              Image.asset(
+                'lib/menu/menubody.png',
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+              ),
+              Image.asset(
+                'lib/menu/leg1.jpeg',
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+              ),
+              Image.asset(
+                'lib/menu/aaaa.png',
+                width: double.infinity,
+                fit: BoxFit.fitWidth,
+              ),
+            ],
+          );
+        } else {
+          return Row(
+            children: [
+              Expanded(
+                child: Container(
+                  color: SpaColors.sand,
+                ),
+              ),
+              SizedBox(
+                width: 800,
+                child: ListView(
+                  padding: EdgeInsets.zero,
+                  children: [
+                    Image.asset(
+                      'lib/menu/menu0.png',
+                      width: double.infinity,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    Image.asset(
+                      'lib/menu/menubody.png',
+                      width: double.infinity,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    Image.asset(
+                      'lib/menu/leg1.jpeg',
+                      width: double.infinity,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    Image.asset(
+                      'lib/menu/aaaa.png',
+                      width: double.infinity,
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: const Color(0xFFC78159), // rgba(199, 129, 89)
+                ),
+              ),
+            ],
+          );
+        }
+      },
     );
   }
 }
